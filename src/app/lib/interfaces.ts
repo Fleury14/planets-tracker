ca185fd4ec59d38fabafbd059417a230358e7d93
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2026-01-04 15:52:58 -0800

    chore(data): create key item list object

---
+++ src/app/lib/interfaces.ts
@@ -0,0 +1,35 @@
+interface KI {
+    hasItem: boolean;
+    iconColor: string;
+    iconGrey: string;
+}
+
+interface KIList {
+    spore: KI;
+    kraid: KI;
+    ridley: KI;
+    longBeam: KI;
+    spazerBeam: KI;
+    iceBeam: KI;
+    waveBeam: KI;
+    morphBall: KI;
+    bombs: KI;
+    springBall: KI;
+    varia: KI;
+    highJump: KI;
+    spaceJump: KI;
+    screwAttack: KI;
+    aegis: KI;
+    crystal: KI;
+    chrono: KI;
+    magnet: KI;
+    phantom: KI;
+    phazon: KI;
+    sensor: KI;
+    stun: KI;
+    surge: KI;
+    // capacitor: KI;
+
+}
+
+export type { KIList }
---
+++ src/app/lib/key-items.ts
@@ -0,0 +1,121 @@
+import { KIList } from './interfaces';
+
+const defaultKI: KIList = {
+    spore: {
+        hasItem: false,
+        iconColor: "Boss_SporeSpawn_BW.png",
+        iconGrey: "Boss_SporeSpawn.png",
+    },
+    kraid: {
+        hasItem: false,
+        iconColor: "Boss_Kraid_BW.png",
+        iconGrey: "Boss_Kraid.png",
+    },
+    ridley: {
+        hasItem: false,
+        iconColor: "Boss_Ridey_BW.png",
+        iconGrey: "Boss_Ridley.png",
+    },
+    longBeam: {
+        hasItem: false,
+        iconColor: "Item_LongBeam_BW.png",
+        iconGrey: "Item_LongBeam.png",
+    },
+    spazerBeam: {
+        hasItem: false,
+        iconColor: "Item_SpazerBeam_BW.png",
+        iconGrey: "Item_SpazerBeam.png",
+    },
+    iceBeam: {
+        hasItem: false,
+        iconColor: "Item_IceBeam_BW.png",
+        iconGrey: "Item_IceBeam.png",
+    },
+    waveBeam: {
+        hasItem: false,
+        iconColor: "Item_WaveBeam_BW.png",
+        iconGrey: "Item_WaveBeam.png",
+    },
+    morphBall: {
+        hasItem: false,
+        iconColor: "Item_MorphBall_BW.png",
+        iconGrey: "Item_MorphBall.png",
+    },
+    bombs: {
+        hasItem: false,
+        iconColor: "Item_Bombs_BW.png",
+        iconGrey: "Item_Bombs.png",
+    },
+    springBall: {
+        hasItem: false,
+        iconColor: "Item_SpringBall_BW.png",
+        iconGrey: "Item_SpringBall.png",
+    },
+    varia: {
+        hasItem: false,
+        iconColor: "Item_VariaSuit_BW.png",
+        iconGrey: "Item_VariaSuit.png",
+    },
+    highJump: {
+        hasItem: false,
+        iconColor: "Item_HighJump_BW.png",
+        iconGrey: "Item_HighJump.png",
+    },
+    spaceJump: {
+        hasItem: false,
+        iconColor: "Item_SpaceJump_BW.png",
+        iconGrey: "Item_SpaceJump.png",
+    },
+    screwAttack: {
+        hasItem: false,
+        iconColor: "Item_ScrewAttack_BW.png",
+        iconGrey: "Item_ScrewAttack.png",
+    },
+    aegis: {
+        hasItem: false,
+        iconColor: "Item_AegisCore_BW.png",
+        iconGrey: "Item_AegisCore.png",
+    },
+    crystal: {
+        hasItem: false,
+        iconColor: "Item_CrystalCore_BW.png",
+        iconGrey: "Item_CrystalCore.png",
+    },
+    chrono: {
+        hasItem: false,
+        iconColor: "Item_ChronoCore_BW.png",
+        iconGrey: "Item_ChronoCore.png",
+    },
+    magnet: {
+        hasItem: false,
+        iconColor: "Item_MagnetCore_BW.png",
+        iconGrey: "Item_MagnetCore.png",
+    },
+    phantom: {
+        hasItem: false,
+        iconColor: "Item_PhantomCore_BW.png",
+        iconGrey: "Item_PhantomCore.png",
+    },
+    phazon: {
+        hasItem: false,
+        iconColor: "Item_PhazonCore_BW.png",
+        iconGrey: "Item_PhazonCore.png",
+    },
+    sensor: {
+        hasItem: false,
+        iconColor: "Item_SensorCore_BW.png",
+        iconGrey: "Item_SensorCore.png",
+    },
+    stun: {
+        hasItem: false,
+        iconColor: "Item_StunCore_BW.png",
+        iconGrey: "Item_StunCore.png",
+    },
+    surge: {
+        hasItem: false,
+        iconColor: "Item_SurgeCore_BW.png",
+        iconGrey: "Item_SurgeCore.png",
+    },
+}
+
+export { defaultKI };
