4b65ab7ccd4f190292b8e77b0df12ad5a409534b
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2026-01-27 13:13:14 -0800

    refac(tracker): seperate cores to look neat

--- src/app/ui/ki-component.tsx
+++ src/app/ui/ki-component.tsx
@@ -7,18 +7,21 @@ const vt = VT323({
     subsets: ['latin'],
 })
 
-export default function KeyItem({ ki, name, toggle }: { ki: KI, name: string, toggle: (key: string) => void}) {
-    if (name==="longBeam") console.log(name, ki)
+export default function KeyItem({ ki, name, toggle, cores }: { ki: KI, name: string, toggle: (key: string) => void, cores: boolean}) {
+    if (cores && !ki.coreText) return null;
+    if (!cores && !!ki.coreText) return null;
     return (
-        <a onClick={() => toggle(name)}>
-            <Image
-                alt={`${ki.hasItem ? "color" : "grey"} icon for ${name}`}
-                src={`/images/${ki.iconGrey}`}
-                height={50}
-                width={50}
-                className={ki.hasItem ? "" : "grayscale"}
-            />
-            {ki.coreColor && ki.coreText && <p className={`${vt.className} ${ki.coreColor} text-md text-center ${ki.hasItem ? "" : "grayscale"}`}>{ki.coreText}</p>}
-        </a>
+        <div className="tracker-ki">
+            <a onClick={() => toggle(name)}>
+                <Image
+                    alt={`${ki.hasItem ? "color" : "grey"} icon for ${name}`}
+                    src={`/images/${ki.iconGrey}`}
+                    height={50}
+                    width={50}
+                    className={ki.hasItem ? "" : "grayscale"}
+                />
+                {ki.coreColor && ki.coreText && <p className={`${vt.className} ${ki.coreColor} text-md text-center ${ki.hasItem ? "" : "grayscale"}`}>{ki.coreText}</p>}
+            </a>
+        </div>
     );
 }
